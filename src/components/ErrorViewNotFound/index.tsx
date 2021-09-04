3f29ff3e4e1684bc5d2c45ce45311a420aad7c77
egorkutz <maxion03@gmail.com>
2021-09-04 11:45:00 +0300

    Rewrite style of errorview

--- src/components/ErrorViewNotFound/index.tsx
+++ src/components/ErrorViewNotFound/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './index.css';
+import style from './index.module.css';
 import {Link} from "react-router-dom";
 
 const ErrorViewNotFound = () => {
-    return <div className={'search_view_container'}>
-        <div className={'search_view'}>
+    return <div className={style.error_view_container}>
+        <div className={style.error_view}>
             <div>
-                <h1 className={'error_code'}>404</h1>
-                <h1 className={'error_title'}>Кажется, такой страницы нет</h1>
-                <span className={'alternative_text'}>Конец - это всегда только начало. Попробуйте <Link className={'link'} to={'/'}>вернуться на главную страницу</Link></span>
+                <h1 className={style.error_code}>404</h1>
+                <h1 className={style.error_title}>Кажется, такой страницы нет</h1>
+                <span className={style.alternative_text}>Конец - это всегда только начало. Попробуйте <Link className={'link'} to={'/'}>вернуться на главную страницу</Link></span>
             </div>
         </div>
     </div>;
